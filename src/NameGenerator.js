e8ed370de31bd2b6f1b574e7260f89a7835d8795
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-01-31 11:16:47 -0800

    added random variable generator

--- src/NameGenerator.js
+++ src/NameGenerator.js
@@ -1,8 +1,36 @@
-var blank = (function () {
-    function blank() {
-    }
+//file for randomly generating variable names
 
-    blank.prototype.method = function () {
-    };
-    return blank;
-})();
+var namefrags = {
+    "con": "bcdfghjklmnpqrstvwxz".split(""),
+    "vow": "aeiou".split("")
+};
+
+
+//gets a random number between 1 and n
+var randnum = function(n) {
+    return Math.floor((Math.random() * n) + 1);
+};
+//randomly selects an array element
+var randselect = function(arr) {
+    return arr[Math.floor((Math.random() * arr.length))];
+};
+//randomly picks value from object
+var randkeyval = function(obj) {
+    var select = Object.keys(obj);
+    return obj[randselect(select)];
+};
+//generates easy to read random variable name
+var randname = function(length) {
+    var name = "";
+    while (name.length<length) {
+        var syllen = Math.floor((Math.random() * 3) + 2);
+        name += randselect(namefrags.con);
+        var randvow = randselect(namefrags.vow);
+        if(syllen>2) name += randvow + randvow;
+        else name += randvow;
+    }
+    return name;
+};
+
+//exports random var func
+exports.randname = randname;
