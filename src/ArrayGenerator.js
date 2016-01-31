306707f11e9524d4cda28a82fb2c2a789087c21c
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-01-31 14:11:20 -0800

    math and arrays

---
+++ src/ArrayGenerator.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Josh on 1/31/16.
+ */
+var ng = require("./NameGenerator.js");
+//file for generating random arrays of strings and numbers
+
+
--- src/MathGenerator.js
+++ src/MathGenerator.js
@@ -1,3 +1,5 @@
+var ng = require("./NameGenerator.js");
+
 //File for randomly creating math expressions
 
 //generates string of random number
@@ -11,6 +13,7 @@ var randselect = function(arr) {
 };
 
 var mathsymbols = "+-*/%".split("");
+var equalsymbols = ["+=", "-=", "*=", "/="];
 //forms random string of arithmetic expression.
 var randmath = function(length) {
     var expr = randstrnum(30);
@@ -19,5 +22,22 @@ var randmath = function(length) {
     }
     return expr;
 };
-
 exports.randmath = randmath;
+
+// returns string for random math function, of one line
+var randommathfunc = function (n) {
+    var param = ng.randname(4);
+    var funcstring = "function " + ng.randname(4) + "(" + param + ") {\n   return ";
+    funcstring += param;
+    var count = 0;
+    while(count<n) {
+        count += 1;
+        var rand = Math.floor((Math.random() * 4) + 1);
+        if(rand>2) funcstring += randselect(mathsymbols) + param;
+        else funcstring += randselect(mathsymbols) + randstrnum(30);
+    }
+    funcstring += ";\n}";
+    return funcstring;
+};
+//exports the random math function
+exports.randommathfunc = randommathfunc;
