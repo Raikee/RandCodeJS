43a33e3006e21f1da1f01805a3837bc552043dc1
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-02-04 13:03:33 -0800

    updates

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
 
-
+var fs = require("fs");
 var ng = require("./src/NameGenerator.js");
 var arrwriter = require("./src/ArrayMachine.js");
+var fname = process.argv[2];
+var fcount = process.argv[3];
 
-console.log(arrwriter.ArrayMachine(6));
+function makefunctions(count) {
+    var filestring = "";
+    for(var i=0;i<count;i++) filestring += arrwriter.ArrayMachine(6) + "\n\n";
+    return filestring;
+}
+
+fs.writeFile(fname, makefunctions(parseInt(fcount)), function(err){
+    if(err) throw err;
+    console.log("Array Function file has been written");
+});
+
